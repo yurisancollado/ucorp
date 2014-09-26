0aadc8c8e84e32c1bcc74bdc9e150f0c6e0bfa37
Yurisan Collado <yurisan29@hotmail.com>
2014-09-25 22:51:08 -0430

    Login

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -5,8 +5,9 @@
  * It contains the authentication method that checks if the provided
  * data can identity the user.
  */
-class UserIdentity extends CUserIdentity
-{
+class UserIdentity extends CUserIdentity {
+	private $_id;
+	const ERROR_DISABLED = 69;
 	/**
 	 * Authenticates a user.
 	 * The example implementation makes sure if the username and password
@@ -15,19 +16,19 @@ class UserIdentity extends CUserIdentity
 	 * against some persistent user identity storage (e.g. database).
 	 * @return boolean whether authentication succeeds.
 	 */
-	public function authenticate()
-	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		elseif($users[$this->username]!==$this->password)
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else
-			$this->errorCode=self::ERROR_NONE;
-		return !$this->errorCode;
+	public function authenticate() {
+		$usuario = new SoapClient('http://localhost:81/webservice/index.php?r=usuario/quote');
+		if ($usuario -> getExisteUsuario($this -> username) == 0)
+			$this -> errorCode = self::ERROR_USERNAME_INVALID;
+		else if ($usuario -> getValidarUsuario($this -> username, $this -> password)== -1)
+			$this -> errorCode = self::ERROR_PASSWORD_INVALID;
+		else {
+			$this -> _id = $usuario -> getValidarUsuario($this -> username, $this -> password);
+			$this -> username = $this -> username;
+			$this -> errorCode = self::ERROR_NONE;
+			$this -> setState('tipoUsuario', $usuario -> getTipoUsuario($this -> username));
+		}
+		return $this -> errorCode == self::ERROR_NONE;
 	}
+
 }
